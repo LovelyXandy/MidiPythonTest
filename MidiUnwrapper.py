fb0171c823ab5ef0df90bf7cd217307da4e726fb
LovelyXandy <60007333+LovelyXandy@users.noreply.github.com>
2020-12-20 11:11:00 +0000

    added bits, untested

--- MidiUnwrapper.py
+++ MidiUnwrapper.py
@@ -3,12 +3,13 @@ from mido import MidiFile
 
 mid = MidiFile('LevanPolka.mid')
 
-#Constants and simple lambda for getting the frequency of a note - note 69 is 440 / middle C - every octave doubles frequency
+#Constants and simple lambda for getting the frequency of a note - note 69 is 440 / concert A - every octave doubles frequency
 g = 2**(1/12)
 f = lambda midi: 440*g**(midi-69)
 #    freq = f(midi)
 
 listTrackData = []
+#convert note events to a list of dictionaries
 for i, track in enumerate(mid.tracks):
     print('Track {}: {}'.format(i, track.name))
     for msg in track:
@@ -21,4 +22,20 @@ for i, track in enumerate(mid.tracks):
                 dictAdd[dp[0]] = dp[1]
             listTrackData.append(dictAdd)
 
-print(listTrackData)
+#print(listTrackData)
+
+noteList = []
+rollDict = {}
+
+for noteEvent in listTrackData:
+    if(noteEvent["identifier"] == "note_on"):
+        if(noteEvent["note"]+"on" in rollDict):
+            rollDict[noteEvent["note"]+"on"].append = noteEvent.values()
+        else:
+            rollDict[noteEvent["note"]+"on"] = [noteEvent.values()]
+    else:
+        if(noteEvent["note"]+"off" in rollDict):
+            rollDict[noteEvent["note"]+"off"].append = noteEvent.values()
+        else:
+            rollDict[noteEvent["note"]+"off"] = [noteEvent.values()]
+    
