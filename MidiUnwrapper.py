bb1a7624e87902915531eb828bd6c862ff70bde7
LovelyXandy <60007333+LovelyXandy@users.noreply.github.com>
2020-12-21 21:59:10 +0000

    Working with simple Midi
    levPolkXand is a simple midi file that can be used as an example

--- MidiUnwrapper.py
+++ MidiUnwrapper.py
@@ -1,20 +1,23 @@
 import mido
 from mido import MidiFile
 
-mid = MidiFile('LevanPolka.mid')
+mid = MidiFile('levPolkXand.mid')
 outputFile = open("output.txt", mode="w")
 
 #Constants and simple lambda for getting the frequency of a note - note 69 is 440 / concert A - every octave doubles frequency
 g = 2**(1/12)
 f = lambda midi: 440*g**(midi-69)
 #    freq = f(midi)
-toms = 5
+toms = 2
+defDuration = 100
+minNote = 0
+minPause = 0
 
 listTrackData = []
 position = 0
 #convert note events to a list of dictionaries
 for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
+    print('Track {}: {} {}'.format(i, track.name, len(track)))
     if (i == 1):
         for msg in track:
             data = str(msg).split(" ")
@@ -28,7 +31,7 @@ for i, track in enumerate(mid.tracks):
                 dictAdd["abs"] = position
                 listTrackData.append(dictAdd)
 
-#print(listTrackData)
+print(listTrackData[0:10])
 
 noteList = []
 rollDict = {}
@@ -46,14 +49,26 @@ for noteEvent in listTrackData:
             rollDict[noteEvent["note"]+"off"] = [noteEvent]
     
 #print(rollDict["42on"])
-#print(rollDict["42off"])
+#print(rollDict["78off"])
 
     
 for noteEvent in listTrackData:
     if(noteEvent["identifier"] == "note_on"):
-        noteEvent["duration"] = rollDict[noteEvent["note"]+"off"].pop(0)["abs"]-rollDict[noteEvent["note"]+"on"].pop(0)["abs"]
-        noteList.append(noteEvent)
+        try:
+            noteEvent["duration"] = rollDict[noteEvent["note"]+"off"].pop(0)["abs"]-rollDict[noteEvent["note"]+"on"].pop(0)["abs"]
+            noteList.append(noteEvent)
+        except:
+            #print("No matching off ", str(noteEvent["note"]))
+            noteEvent["duration"] = defDuration
+            noteList.append(noteEvent)
+
+print(len(noteList))
 
 for i, j in zip(noteList, noteList[1:]):
-    outputFile.write("music.play_tone(" + str(int(f(int(i["note"])))) + ", " + str(int(i["duration"])*toms) + ")\n")
-    outputFile.write("basic.pause("+str((j["abs"]-i["abs"])*toms)+")\n")
+    if(int(i["note"]) >= minNote):
+        outputFile.write("music.play_tone(" + str(int(f(int(i["note"])))) + ", " + str(int(i["duration"])*toms) + ")\n")
+
+    if(minPause <= ((j["abs"]-i["abs"])*toms)):
+        outputFile.write("basic.pause("+str((j["abs"]-i["abs"])*toms)+")\n")
+
+outputFile.close()
